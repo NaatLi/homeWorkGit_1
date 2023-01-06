73e81b8716a2ad36b5d6bd70df7e97215162f0d7
NaatLi <naat.li@yandex.ru>
2023-01-06 12:25:13 +0300

    Добавление класса Main и файла README.md

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {10, 45, 56};
+
+        SalesManager salesManager = new SalesManager(sales);
+
+        int bestSales = salesManager.max();
+
+        System.out.println(bestSales);
+    }
 }
